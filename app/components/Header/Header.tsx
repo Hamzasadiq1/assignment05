87ac8a855b80b708a5dc8adb7a227b9c492623ab
Muhammad Hamza <hamzamohammadhamza153@gmail.com>
2024-11-10 15:21:20 +0500

    hello

---
+++ app/components/Header/Header.tsx
@@ -0,0 +1,19 @@
+
+import React from 'react'
+
+
+
+const Header = () => {
+  return (
+    <div className='bg-[#A29875] w-[1920px] h-[134px] flex'>
+       
+    <div className=' ml-[56.76px]'> 
+   <h1 className=' w-[439px] h-[94px] font-Rye font-bold text-[75px] text-white ' >MANZZARI</h1>    
+       </div>
+       
+
+       </div>
+  )
+}
+
+export default Header
